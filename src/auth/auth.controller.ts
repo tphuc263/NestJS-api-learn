91cc7023251883a522a73191b0e02f9482b0fc73
tphuc263 <tphuc.ptit@gmail.com>
2025-06-26 23:16:43 +0700

    update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,7 +16,7 @@ export class AuthController {
   }
 
   @Post('/signin')
-  signin() {
-    return 'I am signin in';
+  signin(@Body() dto: AuthDto) {
+    return this.authService.login(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -10,16 +14,61 @@ export class AuthService {
     // generate password hash
     const hash = await argon.hash(dto.password);
     // save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+        },
+      });
 
-    // return saved user
-    return user;
+      // return saved user
+      return user;
+    } catch (err) {
+      if (
+        err instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (err.code == 'P2002') {
+          throw new ForbiddenException(
+            'Email be used!',
+          );
+        }
+      }
+      throw err;
+    }
   }
 
-  login() {}
+  async login(dto: AuthDto) {
+    // find the user by email
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+    // if user does not exist throw exception
+    if (!user)
+      throw new ForbiddenException(
+        'email not found',
+      );
+
+    // compare password
+    const isMatchPw = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+    // if password incorrect throw exception
+    if (!isMatchPw)
+      throw new ForbiddenException(
+        'password incorrect',
+      );
+    const { hash, ...res } = user;
+    // send back the user
+    return res;
+  }
 }
